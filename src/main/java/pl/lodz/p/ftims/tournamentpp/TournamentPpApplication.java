2c93fb9c472e55213748e95b23d1a5a39b5a5d71
Michał Sośnicki <sosnicki.michal@gmail.com>
2016-04-15 21:33:34 +0200

    Configure checkstyle plugin. Fix warnings.

--- src/main/java/pl/lodz/p/ftims/tournamentpp/TournamentPpApplication.java
+++ src/main/java/pl/lodz/p/ftims/tournamentpp/TournamentPpApplication.java
@@ -6,7 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TournamentPpApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TournamentPpApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TournamentPpApplication.class, args);
+    }
+
 }
