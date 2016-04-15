9cb3a5ae7885a8996de1d96f44ee342bd525edee
Michał Sośnicki <sosnicki.michal@gmail.com>
2016-04-15 18:06:28 +0200

    Project boot

---
+++ src/main/java/pl/lodz/p/ftims/tournamentpp/TournamentPpApplication.java
@@ -0,0 +1,12 @@
+package pl.lodz.p.ftims.tournamentpp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TournamentPpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TournamentPpApplication.class, args);
+	}
+}
