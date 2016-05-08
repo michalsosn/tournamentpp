7f871f2b134d2e6297104d23dcf867897aad15a7
danpe <dpeczek93@gmail.com>
2016-05-08 21:33:18 +0200

    Wprowadzenie poprawek pozwalających na uruchomienie aplikacji

--- src/main/java/pl/lodz/p/ftims/tournamentpp/entities/GameEntity.java
+++ src/main/java/pl/lodz/p/ftims/tournamentpp/entities/GameEntity.java
@@ -5,11 +5,7 @@
  */
 package pl.lodz.p.ftims.tournamentpp.entities;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import javax.persistence.CollectionTable;
 import javax.persistence.Column;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
@@ -18,9 +14,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
-import javax.persistence.MapKeyColumn;
 import javax.persistence.MapKeyJoinColumn;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
