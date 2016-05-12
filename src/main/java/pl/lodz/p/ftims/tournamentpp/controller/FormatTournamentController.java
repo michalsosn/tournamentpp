c9190c8f9650e42d57d3e9746a54d870be13fa3e
danpe <dpeczek93@gmail.com>
2016-05-12 13:54:04 +0200

    Przygotowanie osobnego serwisu restowego dla pobierania rund turnieju, rozpoczęcie pracy nad główną formatką dla drzewka

--- src/main/java/pl/lodz/p/ftims/tournamentpp/controller/FormatTournamentController.java
+++ src/main/java/pl/lodz/p/ftims/tournamentpp/controller/FormatTournamentController.java
@@ -1,35 +1,29 @@
 package pl.lodz.p.ftims.tournamentpp.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-import pl.lodz.p.ftims.tournamentpp.entities.RoundEntity;
-import pl.lodz.p.ftims.tournamentpp.service.FormatTournamentService;
-
-import java.util.List;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Created by dpp on 5/7/16.
  */
 @Controller
-@RestController
 public class FormatTournamentController {
 
-    @Autowired
-    private FormatTournamentService formatTournamentService;
 
-    @RequestMapping  (path = "/single", method = RequestMethod.GET)
+    @RequestMapping(path = "/single", method = RequestMethod.GET)
     public String showExample() {
         return "singleElimination";
     }
 
-    @RequestMapping (path = "/{tournamentId}/tree",
+    @RequestMapping(path = "/{tournamentId}/tree",
             method = RequestMethod.GET,
             produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<RoundEntity> getTournamentTree(@PathVariable Long tournamentId) {
-        List<RoundEntity> rounds = formatTournamentService.getRounds(tournamentId);
-        return rounds;
+    public String getTournamentTree(@PathVariable Long tournamentId, Model model) {
+        model.addAttribute("tournamentId", tournamentId);
+        return "formatTree";
     }
-
 }
