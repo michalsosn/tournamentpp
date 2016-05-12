b8f02510df6dd5c57dd12b0f99e600e4a0c82ec4
danpe <dpeczek93@gmail.com>
2016-05-12 13:20:41 +0200

    Stworzenie usług pozwalających na pobranie rund przed REST

--- src/main/java/pl/lodz/p/ftims/tournamentpp/controller/FormatTournamentController.java
+++ src/main/java/pl/lodz/p/ftims/tournamentpp/controller/FormatTournamentController.java
@@ -1,28 +1,35 @@
 package pl.lodz.p.ftims.tournamentpp.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
+import pl.lodz.p.ftims.tournamentpp.entities.RoundEntity;
 import pl.lodz.p.ftims.tournamentpp.service.FormatTournamentService;
+
+import java.util.List;
 
 /**
  * Created by dpp on 5/7/16.
  */
 @Controller
+@RestController
 public class FormatTournamentController {
 
     @Autowired
     private FormatTournamentService formatTournamentService;
 
-    @RequestMapping(path = "/single", method = RequestMethod.GET)
+    @RequestMapping  (path = "/single", method = RequestMethod.GET)
     public String showExample() {
         return "singleElimination";
     }
 
-    @RequestMapping(path = "/{tournament_id}/tree", method = RequestMethod.GET)
-    public String getTournamentTree(){
-        return "";
+    @RequestMapping (path = "/{tournamentId}/tree",
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<RoundEntity> getTournamentTree(@PathVariable Long tournamentId) {
+        List<RoundEntity> rounds = formatTournamentService.getRounds(tournamentId);
+        return rounds;
     }
 
 }
--- src/main/java/pl/lodz/p/ftims/tournamentpp/service/FormatTournamentService.java
+++ src/main/java/pl/lodz/p/ftims/tournamentpp/service/FormatTournamentService.java
@@ -2,9 +2,11 @@ package pl.lodz.p.ftims.tournamentpp.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pl.lodz.p.ftims.tournamentpp.entities.RoundEntity;
 import pl.lodz.p.ftims.tournamentpp.repository.TournamentRepository;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 /**
  * Created by Daniel on 2016-05-11.
@@ -15,5 +17,19 @@ public class FormatTournamentService {
 
     @Autowired
     private TournamentRepository tournamentRepository;
+
+    public List<RoundEntity> getRounds(Long tournamentId) {
+        checkIfTournamentExists(tournamentId);
+        List<RoundEntity> rounds = tournamentRepository
+                .findById(tournamentId).get().getRounds();
+        return rounds;
+    }
+
+    private void checkIfTournamentExists(Long tournamentId) {
+        if (!tournamentRepository.findById(tournamentId).isPresent()) {
+            throw new IllegalArgumentException(
+                    "Tournament " + tournamentId + " not exists");
+        }
+    }
 
 }
