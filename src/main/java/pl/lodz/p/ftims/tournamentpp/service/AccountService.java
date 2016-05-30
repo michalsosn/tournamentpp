2df42668ac8952c897008bc0833476fd5da10ce5
Michał Sośnicki <sosnicki.michal@gmail.com>
2016-05-30 23:42:12 +0200

    Integration tests for finding and updating accounts

--- src/main/java/pl/lodz/p/ftims/tournamentpp/service/AccountService.java
+++ src/main/java/pl/lodz/p/ftims/tournamentpp/service/AccountService.java
@@ -27,8 +27,13 @@ public class AccountService {
     @Autowired
     private AccountRepository accountRepository;
 
+    public AccountEntity findAccount() {
+        return findLoggedUser().get();
+    }
+
     public void createAccount(AccountDto account) {
-        checkDto(account);
+        checkPassword(account.getPassword());
+        checkUsernameUnique(account);
         AccountEntity accountEntity = new AccountEntity();
         account.applyToEntity(accountEntity, passwordEncoder);
         accountEntity.setActive(true);
@@ -36,34 +41,37 @@ public class AccountService {
         log.info("Account {} registered", accountEntity.getUsername());
     }
 
-    private void checkDto(AccountDto account) {
-        if (account.getPassword().length() < 4) {
-            throw new IllegalArgumentException(
-                    "User password shorter than 4"
-            );
-        }
-        if (accountRepository.findByUsername(account.getUsername()).isPresent()) {
-            throw new IllegalArgumentException(
-                    "User " + account.getUsername() + " already exists"
-            );
-        }
-    }
-
-    public AccountEntity findAccount() {
-        return findLoggedUser().get();
-    }
-
     public void updateAccount(ProfileDto accountDto) {
-         AccountEntity accountEntity = findLoggedUser().get();
-         accountDto.applyToEntity(accountEntity, passwordEncoder);
-         log.info("Account {} updated", accountEntity.getUsername());
+        checkPassword(accountDto.getPassword());
+        AccountEntity accountEntity = findLoggedUser().get();
+        accountDto.applyToEntity(accountEntity, passwordEncoder);
+        log.info("Account {} updated", accountEntity.getUsername());
     }
 
     private Optional<AccountEntity> findLoggedUser() {
         Authentication authentication
                 = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null) {
+            return Optional.empty();
+        }
         String username = authentication.getName();
         return accountRepository.findByUsername(username);
+    }
+
+    private void checkPassword(String password) {
+        if (password != null && password.length() < 4) {
+            throw new IllegalArgumentException(
+                    "User password shorter than 4"
+            );
+        }
+    }
+
+    private void checkUsernameUnique(AccountDto account) {
+        if (accountRepository.findByUsername(account.getUsername()).isPresent()) {
+            throw new IllegalArgumentException(
+                    "User " + account.getUsername() + " already exists"
+            );
+        }
     }
 
 }
