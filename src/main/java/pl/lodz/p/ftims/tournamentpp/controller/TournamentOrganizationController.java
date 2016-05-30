dc39ab5938080f59f92015815a60ef7a162f01fc
Vertig00 <kluchus@gmail.com>
2016-05-30 23:07:47 +0200

    clear

--- src/main/java/pl/lodz/p/ftims/tournamentpp/controller/TournamentOrganizationController.java
+++ src/main/java/pl/lodz/p/ftims/tournamentpp/controller/TournamentOrganizationController.java
@@ -1,7 +1,5 @@
 package pl.lodz.p.ftims.tournamentpp.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
@@ -10,7 +8,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+
 import pl.lodz.p.ftims.tournamentpp.entities.TournamentEntity;
+import pl.lodz.p.ftims.tournamentpp.repository.TournamentDataTableRepository;
 import pl.lodz.p.ftims.tournamentpp.service.TournamentService;
 
 /**
@@ -22,27 +22,42 @@ public class TournamentOrganizationController {
     @Autowired
     private TournamentService tournamentService;
 
-    // TODO to może być przydatne do wyświetlenia listy turniejów!
-//    @RequestMapping(path = "/tournament/tournaments", method = RequestMethod.GET)
-//    public String listTournaments(
-//            @RequestParam(name = "page", defaultValue = "0") Integer page,
-//            Model model
-//    ) {
-//        final Page<TournamentEntity> tournaments
-//                = tournamentService.listTournaments(page);
-//        model.addAttribute("tournaments", tournaments);
-//        return "/tournament/tournaments";
-//    }
+    @Autowired
+    private TournamentDataTableRepository tournamentDataTableRepository;
 
+    // TODO to może być przydatne do wyświetlenia listy turniejów!
     @RequestMapping(path = "/tournament/tournaments", method = RequestMethod.GET)
-    public String listTournaments(Model model) {
-        final Iterable<TournamentEntity> tournaments =
-                       tournamentService.listAllTournaments();
+    public String listTournaments(
+            @RequestParam(name = "page", defaultValue = "0") Integer page,
+            Model model
+    ) {
+        final Page<TournamentEntity> tournaments
+                = tournamentService.listTournaments(page);
         model.addAttribute("tournaments", tournaments);
         return "/tournament/tournaments";
     }
 
-    // TODO to może być przydatne do wyświetlenia turnieju
+//    @JsonView(DataTablesOutput.View.class)
+//    @RequestMapping(value = "/tournament/tournaments", method = RequestMethod.GET)
+//    public DataTablesOutput<TournamentEntity> getUsers(@Valid DataTablesInput input) {
+//        return tournamentDataTableRepository.findAll(input);
+//    }
+
+//    @JsonView(DataTablesOutput.View.class)
+//    @RequestMapping(path = "/tournament/tournaments", method = RequestMethod.GET)
+//    public String listTournaments(Model model, DataTablesInput input) {
+//        final DataTablesOutput<TournamentEntity> tournaments =
+//              tournamentDataTableRepository.findAll(input);
+//        model.addAttribute("tournaments", tournaments);
+//        return "/tournament/tournaments";
+//    }
+
+//    @JsonView(DataTablesOutput.View.class)
+//    @RequestMapping(path = "/tournament/tournaments", method = RequestMethod.GET)
+//    public DataTablesOutput<TournamentEntity> getUsers(@Valid DataTablesInput input) {
+//        return tournamentDataTableRepository.findAll(input);
+//    }
+
     @RequestMapping(path = "/tournament/tournament/{id}", method = RequestMethod.GET)
     public String getTournament(@PathVariable long id, Model model) {
         final TournamentEntity tournament = tournamentService.findTournament(id);
