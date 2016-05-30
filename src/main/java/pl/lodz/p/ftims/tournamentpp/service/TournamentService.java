72a2d168cc8f694b7bb7646677cf301147d0220f
Vertig00 <kluchus@gmail.com>
2016-05-30 19:28:33 +0200

    Przechodzenie między formularzami

--- src/main/java/pl/lodz/p/ftims/tournamentpp/service/TournamentService.java
+++ src/main/java/pl/lodz/p/ftims/tournamentpp/service/TournamentService.java
@@ -1,7 +1,5 @@
 package pl.lodz.p.ftims.tournamentpp.service;
 
-import java.util.List;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
