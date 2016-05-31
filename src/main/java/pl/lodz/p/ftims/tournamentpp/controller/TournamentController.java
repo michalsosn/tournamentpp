82ba3322461659878953ea3f4c0196444519545a
Nex0Zero <180550@edu.p.lodz.pl>
2016-05-31 10:29:38 +0200

    Logika update delete

--- src/main/java/pl/lodz/p/ftims/tournamentpp/controller/TournamentController.java
+++ src/main/java/pl/lodz/p/ftims/tournamentpp/controller/TournamentController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -46,6 +47,44 @@ public class TournamentController {
         return "redirect:/createTournament";
     }
 
+    @RequestMapping(path = "/deleteTournament/{tournamentId}",
+            method = RequestMethod.POST)
+    public String deleteTournament(@PathVariable long tournamentId) {
+
+        tournamentService.deleteTournament(tournamentId);
+
+        /*
+         * TODO
+         * na co przekierowywac
+         */
+        return "redirect:/createTournament";
+    }
+
+    @RequestMapping(path = "/updateTournament/{tournamentId}",
+            method = RequestMethod.GET)
+    public String updateTournament(Model model, @PathVariable long tournamentId) {
+        TournamentDto tournamentDto = tournamentService.getTournamentDto(tournamentId);
+        model.addAttribute("tournament", tournamentDto);
+
+        return "updateTournament";
+    }
+
+    @RequestMapping(path = "/deleteTournament/{tournamentId}",
+            method = RequestMethod.POST)
+    public String updateTournament(@Valid
+            @ModelAttribute("tournament") TournamentDto tournament,
+            BindingResult bindingResult, @PathVariable long tournamentId) {
+        if (bindingResult.hasErrors()) {
+            return "createTournament";
+        }
+        tournamentService.updateTournament(tournament, tournamentId);
+
+        /*
+         * TODO
+         * na co przekierowywac
+         */
+        return "redirect:/createTournament";
+    }
 }
 
 
--- src/main/java/pl/lodz/p/ftims/tournamentpp/service/TournamentService.java
+++ src/main/java/pl/lodz/p/ftims/tournamentpp/service/TournamentService.java
@@ -25,4 +25,29 @@ public class TournamentService {
         log.info("Tournament {} created", tournamentEntity.getId());
     }
 
+    public void deleteTournament(Long id) {
+        tournamentRepository.delete(id);
+        log.info("Tournament id:{} created", id);
+    }
+
+    public void updateTournament(TournamentDto tournament, Long tournamentId) {
+        TournamentEntity tournamentEntity = tournamentRepository.findOne(tournamentId);
+        tournament.applyToEntity(tournamentEntity);
+        tournamentRepository.save(tournamentEntity);
+        log.info("Tournament {} edited", tournamentEntity.getId());
+    }
+
+    public TournamentDto getTournamentDto(Long id) {
+        TournamentDto tournamentDto = new TournamentDto();
+        TournamentEntity tournamentEntity = tournamentRepository.findOne(id);
+
+        tournamentDto.setDescription(tournamentEntity.getDescription());
+        tournamentDto.setFormat(tournamentEntity.getFormat());
+        tournamentDto.setLocation(tournamentEntity.getLocation());
+        tournamentDto.setOrganizer(tournamentEntity.getOrganizer());
+        tournamentDto.setStartTime(tournamentEntity.getStartTime());
+
+        return tournamentDto;
+    }
+
 }
